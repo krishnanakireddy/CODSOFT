0121a93b3e63cf74ea2741f8192691ef4fca863f
Nakireddy Sai Durga Madhava Krishna Teja <krishnatejanakireddy@gmail.com>
2026-02-03 20:47:09 +0530

    Add files via upload

---
+++ Python/calculator.py
@@ -0,0 +1,29 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+while True:
+    print("\nSelect operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit/n")
+
+    choice = input("Enter choice (1/5): ")
+
+    if choice == '1':
+        print(f"{num1} + {num2} = {num1 + num2}")
+    elif choice == '2':
+        print(f"{num1} - {num2} = {num1 - num2}")
+    elif choice == '3':
+        print(f"{num1} * {num2} = {num1 * num2}")
+    elif choice == '4':
+        if num2 != 0:
+            print(f"{num1} / {num2} = {num1 / num2}")
+        else:
+            print("Error! Division by zero.")
+    elif choice == '5':
+        print("Exiting the calculator.")
+        break
+    else:
+        print("Invalid input")
